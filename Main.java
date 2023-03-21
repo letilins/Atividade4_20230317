45652c64a534eabb44ebbbdd000ecf608a695981
Letícia Lins <leticialins.rj@gmail.com>
2023-03-20 21:46:56 -0300

    alterando o codigo de Main.

--- Main.java
+++ Main.java
@@ -2,12 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Veiculo veiculo = new Veiculo("Honda", "New City", 2023, "Prata");
+        //Veiculo veiculo = new Veiculo("Honda", "New City", 2023, "Prata");
+        
         Carro carro = new Carro("Chevrolet", "Montana", 2023, "Preto", 4);
         Motocicleta moto = new Motocicleta("Yamaha", "R3 ABS", 2023, "Azul", 321);
 
-        veiculo.imprimirInformacoes();
-        System.out.println();
+        //veiculo.imprimirInformacoes();
+        //System.out.println();
+
+
         moto.imprimirInformacoes();
         System.out.println();
         carro.imprimirInformacoes();
